7ef61f2be6d0ce26ff78218cb9c1c78dd0c479ec
IZeonI <osvaldomartinezespinosa@gmail.com>
2025-05-16 09:32:17 -0600

    1

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Navbar from '../components/Navbar';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '2rem' }}>
+        <h1>Landing page principal</h1>
+        {/* ...contenido... */}
+      </main>
+    </>
+  );
+}
